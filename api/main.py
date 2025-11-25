467daa9b60df4701e5fec596377acaf39d56e554
Deepshikha <148853706+deepshikha04yadav@users.noreply.github.com>
2025-11-26 01:01:40 +0530

    Update CORS middleware to allow all origins

--- api/main.py
+++ api/main.py
@@ -13,10 +13,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],    # Allow POST, GET, OPTIONS etc.
-    allow_headers=["*"],    # Allow JSON content-type
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
